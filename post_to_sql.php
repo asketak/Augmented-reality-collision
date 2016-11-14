7ec3b559b97293a47bcc33ed2ad1847c1a2855a2
asketak <simatomas@seznam.cz>
2016-11-14 17:35:00 +0100

    adding remote menu

--- post_to_sql.php
+++ post_to_sql.php
@@ -3,9 +3,11 @@
 $size = $_POST["size"];
 $object = $_POST["object"];
 $box = $_POST["box"];
-$data = $box . " " . $object . " " . $size
+$data = $box . ":" . $object . ":" . $size;
+$data2 = "huhu";
 $fname = "data.txt";
 
-file_put_contents($fname,$data);
-print($likes_sql);
+$file = fopen($fname,"w");
+echo fwrite($file,$data);
+fclose($file);
 ?>
